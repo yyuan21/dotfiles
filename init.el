fa5cfd91d226450009fd148b782a8098a9f7194e
Yida <yidayuan1028@gmail.com>
2019-03-17 16:34:50 -0400

    switch to melpa main instead of stable

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;; Melpa Package Archive ;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 
