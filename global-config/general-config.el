fa43756d1b69c71928e062522b05048000e6755c
Yida Yuan <yidayuan1028@gmail.com>
2019-03-23 18:40:42 -0400

    config startup window

--- global-config/general-config.el
+++ global-config/general-config.el
@@ -2,3 +2,8 @@
 
 ;; Set auto-pair mode on
 (electric-pair-mode t)
+
+;; maximize frame on start up
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;; open a second window on the right
+(split-window-right)
