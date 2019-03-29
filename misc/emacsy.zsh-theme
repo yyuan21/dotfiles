05bc6a1b4a78c9e3518237ae088222f09acb7981
Yida <yidayuan1028@gmail.com>
2019-03-28 23:27:41 -0400

    configure zsh theme

---
+++ misc/emacsy.zsh-theme
@@ -0,0 +1,40 @@
+# emacsy theme
+
+# collapsed path (like fish)
+_emacsy_collapsed_wd() {
+  echo $(pwd | perl -pe '
+   BEGIN {
+      binmode STDIN,  ":encoding(UTF-8)";
+      binmode STDOUT, ":encoding(UTF-8)";
+   }; s|^$ENV{HOME}|~|g; s|/([^/.])[^/]*(?=/)|/$1|g; s|/\.([^/])[^/]*(?=/)|/.$1|g
+')
+}
+
+if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="green"; fi
+local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
+
+# primary prompt
+PROMPT='$FG[237]------------------------------------------------------------%{$reset_color%}
+$FG[032]$(_emacsy_collapsed_wd)\
+$(git_prompt_info) \
+$FG[105]%(!.#.»)%{$reset_color%} '
+PROMPT2='%{$fg[red]%}\ %{$reset_color%}'
+RPS1='${return_code}'
+
+# color vars
+eval my_gray='$FG[237]'
+eval my_orange='$FG[214]'
+
+# right prompt
+if type "virtualenv_prompt_info" > /dev/null
+then
+	RPROMPT='$(virtualenv_prompt_info)$my_gray%n@%m%{$reset_color%}%'
+else
+	RPROMPT='$my_gray%n@%m%{$reset_color%}%'
+fi
+
+# git settings
+ZSH_THEME_GIT_PROMPT_PREFIX="$FG[075]($FG[078]"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+ZSH_THEME_GIT_PROMPT_DIRTY="$my_orange*%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="$FG[075])%{$reset_color%}"
--- setup.sh
+++ setup.sh
@@ -1,12 +1,42 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+
+MISC_FOLDER=~/.emacs.d/misc/
+
+ZSHRC=~/.zshrc
+OMZ_DIR=~/.oh-my-zsh/
+OMZ_THEME_DIR=$OMZ_DIR/themes/
+
+THEME_FILE=emacsy.zsh-theme
+
+########## setup zsh theme for emacs ##########
+
+# check ~/.zshrc
+if [ ! -f "$ZSHRC" ]; then
+    echo "zshrc not found!"
+    exit 1
+fi
+
+# oh my zsh / themes not installed
+if [ ! -d "$OMZ_DIR" ] || [ ! -d "$OMZ_THEME_DIR" ]; then
+    echo "Make sure oh-my-zsh is correctly installed!"
+    exit 1
+fi
+
+THEME_PATH="$MISC_FOLDER/$THEME_FILE"
+cp "$THEME_PATH" "$OMZ_THEME_DIR"
+
+if [ -f "$OMZ_THEME_DIR/$THEME_FILE" ]; then
+    # check if we successfully copied the theme file
+    echo "emacsy zsh theme installed!"
+fi
+
+# set the theme in zshrc to "emacsy"
+sed -i.origin 's/ZSH_THEME=\".*\"/ZSH_THEME=\"emacsy\"/' "$ZSHRC"
 
 ########## allow zsh to output PWD to emacs ##########
 # the string to check if the code snippet has already
 # been injected into .zshrc
 GREP_STR="# ZSH_EMACS_PWD"
-
-# zshrc file path
-ZSHRC_PATH=~/.zshrc
 
 # the comment of the snippet
 EMACS_PWD_CMT="# allow zsh to output PWD to emacs"
@@ -18,14 +48,14 @@ EMACS_PWD_CODE='if [ -n "$INSIDE_EMACS" ]; then
     }
 fi'
 
-if ! grep -q "$GREP_STR" "$ZSHRC_PATH"; then
+if ! grep -q "$GREP_STR" "$ZSHRC"; then
     # if the code snippnet is not found, inject it
     echo "Configuring zshrc to output PWD to emacs"
-    echo "" >> $ZSHRC_PATH
-    echo "$GREP_STR" >> $ZSHRC_PATH
-    echo "$EMACS_PWD_CMT" >> $ZSHRC_PATH
-    echo "$EMACS_PWD_CODE" >> $ZSHRC_PATH
-    echo "" >> $ZSHRC_PATH
+    echo "" >> "$ZSHRC"
+    echo "$GREP_STR" >> "$ZSHRC"
+    echo "$EMACS_PWD_CMT" >> "$ZSHRC"
+    echo "$EMACS_PWD_CODE" >> "$ZSHRC"
+    echo "" >> "$ZSHRC"
     echo "Done"
 else
     # the code snippet exists
