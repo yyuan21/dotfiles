3c73282a3929c53f509d129c80e966d0cfb8b678
Yida <yidayuan1028@gmail.com>
2019-04-03 00:54:46 -0400

    more thoughts about the setup script

--- setup.sh
+++ setup.sh
@@ -4,15 +4,24 @@
 
 # This script sets up all the dotfiles
 # all the work done here should be idempotent
+# libraries and frameworks are not saved in this repo. Instead
+# they are checked and installed via this script.
+
 # 1. all the original dotfiles should be backed up
 # 2. remove old symbolic links
 # 3. link all the config dotfiles again to the home directory
 
 #################### back up ####################
 
+# only back up the files that have compatibility issues
+# should also have 'unsetup' script to restore all files in the backup folder
+# to exactly where they came from
+
+# The folder contains the original dotfiles
 DOT_BACKUP=~/dotfiles_backup
 
 
+#################### emacs ####################
 
 # the folder that contains the code snippets
 MISC_FOLDER=~/.emacs.d/misc/
